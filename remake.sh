3ddae057fd9b99393120fbe801a71ef90a897617
ChenguangZhang <hi.chenguang@gmail.com>
2019-06-20 21:07:52 -0400

    remove binary lib files

--- remake.sh
+++ remake.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
+echo " ======= make shape ======= "
 cd libshape
 make clean
 make 
+cd ..
+
+echo " ======= make motion ======= "
 cd libmotion
 make clean
 make 
+cd ..
+
+echo " ======= make collision ======= "
 cd libcollision
 make clean
 make 
 cd ..
+
+echo " ======= make sdfibm ======= "
 make clean
 make 
 
