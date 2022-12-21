fbbe3a2c161c7bf5fc007e81319b626affd6ec4b
ChenguangZhang <hi.chenguang@gmail.com>
2022-12-20 20:23:29 -0500

    update factory registry

--- src/libmotion/motionfactory.cpp
+++ src/libmotion/motionfactory.cpp
@@ -33,32 +33,14 @@ std::map<string, MotionFactory::TCreateMethod> MotionFactory::m_methods;
 
 }
 // add motions
-#include "motion000000.h"
-REGISTERMOTION(Motion000000);
-#include "motion000100.h"
-REGISTERMOTION(Motion000100);
-#include "motion000010.h"
-REGISTERMOTION(Motion000010);
-#include "motion000001.h"
-REGISTERMOTION(Motion000001);
-#include "motion000111.h"
-REGISTERMOTION(Motion000111);
 #include "motion000002.h"
 REGISTERMOTION(Motion000002);
 #include "motion110002.h"
 REGISTERMOTION(Motion110002);
-#include "motion111111.h"
-REGISTERMOTION(Motion111111);
 #include "motion222000.h"
 REGISTERMOTION(Motion222000);
 #include "motionsinedirectional.h"
 REGISTERMOTION(MotionSineDirectional);
-#include "motion001001.h"
-REGISTERMOTION(Motion001001);
-#include "motion010010.h"
-REGISTERMOTION(Motion010010);
-#include "motion100100.h"
-REGISTERMOTION(Motion100100);
 #include "motion01mask.h"
 REGISTERMOTION(Motion01Mask);
 #include "motionrotor.h"
